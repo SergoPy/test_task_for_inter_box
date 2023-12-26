f6466f6fb4a62b58f63dfc13a2429c2d344cbb16
ipz214_shchss <ipz214_shchss@student.ztu.edu.ua>
2023-12-26 17:46:11 +0200

    Added scraper

---
+++ ebay_scraper.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def scrape_ebay_images(url):
+    driver = webdriver.Chrome()
+
+    driver.get(url)
+
+    wait = WebDriverWait(driver, 10)
+
+    image_elements = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, "div.ux-image-carousel-item img[data-zoom-src]"))
+    )
+
+    image_urls = set()
+    for image_element in image_elements:
+        image_url = image_element.get_attribute("data-zoom-src")
+        image_urls.add(image_url)
+
+    driver.quit()
+
+    return list(image_urls)
